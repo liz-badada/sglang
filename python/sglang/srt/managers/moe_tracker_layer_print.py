f802cbae20d4e52b5d9b9f4b31793525a7e8c4be
Liz <93358689+liz-badada@users.noreply.github.com>
2025-02-06 11:59:32 +0800

    update

--- python/sglang/srt/managers/moe_tracker_layer_print.py
+++ python/sglang/srt/managers/moe_tracker_layer_print.py
@@ -3,17 +3,23 @@ import torch
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
 from sglang.srt.models.qwen2_moe import Qwen2MoeModel
 from sglang.srt.models.deepseek_v2 import DeepseekV2Model
-from sglang.srt.utils import rank0_print
+
+
+moe_tracker_log = 'moe_tracker_log.txt'
 
 
 def forward_qwen_model_layer_print(self, input_ids: torch.Tensor, positions: torch.Tensor, forward_batch: ForwardBatch, input_embeds: torch.Tensor = None,) -> torch.Tensor:
+    global moe_tracker_log
+    moe_tracker_log = 'qwen_moe_tracker_log.txt'
+
     if input_embeds is None:
         hidden_states = self.embed_tokens(input_ids)
     else:
         hidden_states = input_embeds
     residual = None
     for i in range(len(self.layers)):
-        rank0_print(f"[Qwen]: Layer_{i}")
+        with open(moe_tracker_log, 'a') as file:
+            print(f"[Qwen]: Layer_{i}", file=file)
         layer = self.layers[i]
         hidden_states, residual = layer(
             positions, hidden_states, forward_batch, residual
@@ -23,10 +29,14 @@ def forward_qwen_model_layer_print(self, input_ids: torch.Tensor, positions: tor
 
 
 def forward_deepseek_model_layer_print(self, input_ids: torch.Tensor, positions: torch.Tensor, forward_batch: ForwardBatch,) -> torch.Tensor:
+    global moe_tracker_log
+    moe_tracker_log = 'deepseek_moe_tracker_log.txt'
+    
     hidden_states = self.embed_tokens(input_ids)
     residual = None
     for i in range(len(self.layers)):
-        rank0_print(f"[DeepSeek]: Layer_{i}")
+        with open(moe_tracker_log, 'a') as file:
+            print(f"[DeepSeek]: Layer_{i}", file=file)
         layer = self.layers[i]
         hidden_states, residual = layer(positions, hidden_states, forward_batch, residual)
     if not forward_batch.forward_mode.is_idle():
--- python/sglang/srt/managers/moe_tracker_router_hook.py
+++ python/sglang/srt/managers/moe_tracker_router_hook.py
@@ -1,10 +1,13 @@
-from sglang.srt.utils import rank0_print
+from sglang.srt.managers import moe_tracker_layer_print
 
 
 def moe_select_experts_tracker(func):
     def wrapper(*args, **kwargs):
         topk_weights, topk_ids = func(*args, **kwargs)
-        rank0_print(f"[MoE Router Topk]: weights shape {topk_weights.shape}, ids shape {topk_ids.shape}")
-        rank0_print(f"[MoE Router TopK]: weights {topk_weights}, ids {topk_ids}")
+
+        moe_tracker_log = moe_tracker_layer_print.moe_tracker_log
+        with open(moe_tracker_log, 'a') as file:
+            print(f"[MoE Router Topk]: weights shape {topk_weights.shape}, ids shape {topk_ids.shape}")
+            print(f"[MoE Router TopK]: weights {topk_weights}, ids {topk_ids}")
         return topk_weights, topk_ids
     return wrapper
