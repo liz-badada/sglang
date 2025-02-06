4ac23e49f3f850d6f93275e364440ccec287341c
Liz <93358689+liz-badada@users.noreply.github.com>
2025-02-06 12:07:04 +0800

    update moe log

--- python/sglang/srt/managers/moe_tracker_router_hook.py
+++ python/sglang/srt/managers/moe_tracker_router_hook.py
@@ -1,13 +1,13 @@
-from sglang.srt.managers import moe_tracker_layer_print
+moe_tracker_log = 'moe_tracker_log.txt'
 
 
 def moe_select_experts_tracker(func):
     def wrapper(*args, **kwargs):
         topk_weights, topk_ids = func(*args, **kwargs)
 
-        moe_tracker_log = moe_tracker_layer_print.moe_tracker_log
+        global moe_tracker_log
         with open(moe_tracker_log, 'a') as file:
-            print(f"[MoE Router Topk]: weights shape {topk_weights.shape}, ids shape {topk_ids.shape}")
-            print(f"[MoE Router TopK]: weights {topk_weights}, ids {topk_ids}")
+            print(f"[MoE Router Topk]: weights shape {topk_weights.shape}, ids shape {topk_ids.shape}", file=file)
+            print(f"[MoE Router TopK]: weights {topk_weights}, ids {topk_ids}", file=file)
         return topk_weights, topk_ids
     return wrapper
